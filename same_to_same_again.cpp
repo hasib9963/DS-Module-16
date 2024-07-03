89120440e7fce0b1252ef4e5e4c7885c0e71189b
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-07-04 00:41:59 +0600

    Implement myStack class with push and pop operations

--- same_to_same_again.cpp
+++ same_to_same_again.cpp
@@ -6,3 +6,30 @@ class Node {
         Node* next;
         Node* prev;
 };
+class myStack {
+    public:
+        Node* head = NULL;
+        void push(int val){
+            Node* tmp = new Node();
+            tmp->val = val;
+            tmp->next = head;
+            tmp->prev = NULL;
+            if (head != NULL){
+                head->prev = tmp;
+            }
+            head = tmp;
+        }
+        int pop() {
+            if (head == NULL) {
+                return -1;
+            }
+            Node* tmp = head;
+            int val = tmp->val;
+            head = head->next;
+            if (head != NULL) {
+                head->prev = NULL;
+            }
+            delete(tmp);
+            return val;
+        }
+};
